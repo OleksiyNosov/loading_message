e51bb2b39eb44ff823daae9fcedbf9646e31d36f
Oleksiy Nosov <nosov@mlsdev.com>
2019-07-02 10:07:03 +0300

    Add ability to pass message during loading

--- lib/loading_message.rb
+++ lib/loading_message.rb
@@ -1,5 +1,6 @@
 class LoadingMessage
   attr_reader :min, :max, :step, :message, :loading_index, :finish_message, :start_time, :finish_time
+  attr_reader :previous_message, :current_message, :erase_space
 
   def initialize(min: 0, max: 100, step: 1, message: 'Working:', finish_message: 'Done.')
     @min = min
@@ -11,14 +12,16 @@ class LoadingMessage
     @loading_index = 0.0
   end
 
-  def increment
+  def increment current_message = ''
     @loading_index += 1
     @start_time ||= Time.now
 
-    printf("\r#{ message } %.2f%%", loading_index / max * 100)
+    show_increment current_message
   end
 
   def finish
+    show_increment
+
     rows = [
       started_at,
       finished_at,
@@ -32,6 +35,20 @@ class LoadingMessage
   end
 
   private
+
+  def show_increment current_message = ''
+    @previous_message ||= ''
+    message_diff = previous_message.length - current_message.length
+    @erase_space = message_diff > 0 ? ' ' * message_diff : ''
+
+    printf("\r#{ message } %.2f%% #{ current_message }#{ erase_space }", percent)
+
+    @previous_message = current_message
+  end
+
+  def percent
+    loading_index / max * 100
+  end
 
   def time_took
     delta = finish_time - start_time
