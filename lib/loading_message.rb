fb854a4c671e17e4d45049483b60d7f331d9ef95
Oleksiy Nosov <oleksiynosov@gmail.com>
2020-03-29 23:13:08 +0300

    Added logger

--- lib/loading_message.rb
+++ lib/loading_message.rb
@@ -1,22 +1,36 @@
+require 'json'
+
 class LoadingMessage
   attr_reader :min, :max, :step, :message, :loading_index, :finish_message, :start_time, :finish_time
-  attr_reader :previous_message, :current_message, :erase_space
+  attr_reader :previous_message, :current_message, :erase_space, :increment_data, :standard_logger, :logging
 
-  def initialize(min: 0, max: 100, step: 1, message: 'Working:', finish_message: 'Done.')
+  def initialize(min: 0, max: 100, step: 1, message: 'Working:', finish_message: 'Done.', logging: true)
     @min = min
     @max = max
     @step = step
     @message = message
     @finish_message = finish_message
+    @logging = logging
 
     @loading_index = 0.0
+
+    @standard_logger = LoadingLogger.new('loading_message_standard')
   end
 
-  def increment current_message = ''
+  def increment current_message = '', data: {}
     @loading_index += 1
     @start_time ||= Time.now
+    @increment_data = {
+      loading_index: loading_index.to_i,
+      start_time: start_time,
+      current_time: current_time,
+      percent: string_percent,
+      data: data
+    }
 
     show_increment current_message
+
+    standard_logger.log(increment_data) if logging
   end
 
   def finish
@@ -41,13 +55,20 @@ class LoadingMessage
     message_diff = previous_message.length - current_message.length
     @erase_space = message_diff > 0 ? ' ' * message_diff : ''
 
-    printf("\r#{ message } %.2f%% #{ current_message }#{ erase_space }", percent)
+    printf("\r#{ message } %.2f%% #{ current_message }#{ erase_space }", string_percent)
+
+    @increment_data[:message] = current_message
+    @increment_data[:loading_message] = "#{ message } #{string_percent} #{ current_message }"
 
     @previous_message = current_message
   end
 
   def percent
     loading_index / max * 100
+  end
+
+  def string_percent
+    percent.to_s.gsub(/(?<=\..{2}).*$/, '')
   end
 
   def time_took
@@ -64,8 +85,12 @@ class LoadingMessage
     "Finished at: #{ finish_time }"
   end
 
+  def current_time
+    @current_time ||= Time.now
+  end
+
   def finish_time
-    @finish_time ||= Time.now
+    @finish_time ||= current_time
   end
 
   def duration_distance delta
@@ -81,5 +106,27 @@ class LoadingMessage
     microseconds = (delta - strict_delta) * 1_000_000 % 1_000
 
     format('%02d:%02d:%02d.%03d.%03d', hours, minutes, seconds, miliseconds, microseconds)
+  end
+end
+
+class LoadingLogger
+  attr_reader :time, :filename, :path, :destination, :first_attempt
+
+  def initialize(name, path: nil, time_postfix: true)
+    @time = time_postfix ? Time.now : nil
+    @filename = "#{name}_#{time}.log"
+    @path = path.to_s.gsub(/(?<=.)\/$/, '')
+    @destination = [@path, filename].reject(&:empty?).join('/')
+    @first_attempt = true
+  end
+
+  def log(data)
+    first_write_procedure
+
+    File.open(destination, 'a') { |file| file.puts data.to_json }
+  end
+
+  def first_write_procedure
+    first_attempt && File.write(destination, '') && @first_attempt = false
   end
 end
